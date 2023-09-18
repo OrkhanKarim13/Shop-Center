a03d6a738d458673a1aa339fa61e3ecee9f53acf
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2023-09-18 16:22:38 +0400

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <HeaderVue />
  <router-view></router-view>
+ <TheFooter/>
 </template>
 
 <script>
 import HeaderVue from "@/components/HeaderVue.vue"
+import TheFooter from "@/components/TheFooter.vue"
 
 export default{
   components:{
-    HeaderVue
+    HeaderVue,
+    TheFooter
   }
 }
 
---
+++ src/pages/SingleProduct.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="flex flex-col items-center bg-white-500">
+        <div class=""><h1>{}</h1></div>
+    </section>
+</template>
+
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+
+export default {
+    components: {
+        
+       
+    },
+    methods:{
+        ...mapActions(['fetchPosts']),
+    },
+    computed:{
+        getPost(){
+            return this.$store.state.posts.id
+        },
+        ...mapGetters(['getFakeData']),
+    },
+    created() {
+
+    this.fetchPosts();
+  },
+}
+</script>
